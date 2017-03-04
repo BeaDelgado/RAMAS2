08df95b913b41a3a7679659fc84c82e5040aafd4
Beatriz <Beatriz@Beatriz-VAIO.lan>
2017-03-04 19:53:09 +0100

    PrimeraVersion

---
+++ src/proyecto1/Proyecto1.java
@@ -0,0 +1,17 @@
+
+package proyecto1;
+
+/**
+ *
+ * @author Beatriz
+ */
+public class Proyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("version 1.0");
+    }
+    
+}
